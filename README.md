9b00e07516d9827761759269e27e09ad20ac442d
mzielonko <umzielom@myumanitoba.ca>
2020-02-03 21:25:33 -0600

    First draft of readme

--- README.md
+++ README.md
@@ -1,42 +1,49 @@
-Test parameters:
-  -w https://bblearn.merlin.mb.ca/bbcswebdav/courses/Physics40S_master_08/ -f C:\Users\MZielonko\Documents\Github\Physics-40S -n
-  environment variables:
-  PYTHONIOENCODING="utf-8"
+Blackboard XID to Softlink Converter
+------------------------------------
 
+Version: 1.3.x
+Author: Mark Zielonko
 
-Request headers
----------------
-Accept: image/png, image/svg+xml, image/jxr, image/*; q=0.8, */*; q=0.5
-Accept-Encoding: gzip, deflate
-Accept-Language: en-CA, en; q=0.8, fr-CA; q=0.5, fr; q=0.3
-Cookie: JSESSIONID=1EA71A19BFE157F4B315FB1073C77F77; xythosdrive=0; session_id=32E0DFA32ADF05C28612D6F9DDB655A7; COOKIE_CONSENT_ACCEPTED=true; s_session_id=7093997FF6BE2C29FFF2A143A529B55D; BBAffinity=91a045860796bde3250bad0694879c1df4e424f553e6cab0e4727227c14f6956; JSESSIONID=B149E43E7C06F1EAA9414F3C9303D30D; web_client_cache_guid=96bd3241-4a9d-4543-8bc4-6d81759dc796
-Host: bblearn.merlin.mb.ca
-If-Modified-Since: Wed, 29 Aug 2012 14:50:14 GMT
-If-None-Match: "5118332-DA9F82E4B5CFAF3D610584C03D8AEF2E"
-Referer: https://bblearn.merlin.mb.ca/bbcswebdav/pid-8456659-dt-content-rid-13088596_1/courses/Physics40S_ZielonkoMark_2019/course_content/module1/mod_intro.html?globalNavigation=false
-User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko
+Description: Python script used for replacing all of a selected course's XID
+  links with relative paths based on the XID's redirect address
 
-Response headers
-----------------
-cache-control: no-cache
-content-security-policy: frame-ancestors 'self'
-date: Mon, 30 Dec 2019 20:27:27 GMT
-expires: Sun, 30 Dec 2018 20:27:27 GMT
-last-modified: Thu, 30 Dec 1999 20:27:27 GMT
-p3p: CP="CAO PSA OUR"
-pragma: no-cache
-set-cookie: s_session_id=7093997FF6BE2C29FFF2A143A529B55D; Max-Age=473040000; Expires=Tue, 26-Dec-2034 20:27:27 GMT; Path=/; Secure; HttpOnly
-set-cookie: session_id=32E0DFA32ADF05C28612D6F9DDB655A7; Max-Age=473040000; Expires=Tue, 26-Dec-2034 20:27:27 GMT; Path=/; HttpOnly
-x-blackboard-appserver: blackboard91fe1.merlin.ca
-x-blackboard-product: Blackboard Learn &#8482; 3700.0.2-rel.36+fbf2221
-x-frame-options: SAMEORIGIN
-x-powered-by: ARR/3.0
+Purpose: Allowing existing courses to be converted into courses that are not
+  coupled to Blackboard's proprietary file index system
 
-Name	Protocol	Method	Result	Content type	Received	Time	Initiator
-https://bblearn.merlin.mb.ca/bbcswebdav/courses/Physics40S_master_08/course_content/images/m1_icon.gif	HTTPS	GET	304	image/gif	(from cache)	115.41 ms	document
+Requirements:
+  - python v3.x (can be installed without admin account)
+  - python environment variable access (potentially)
+  - internet connection with no proxy (or results may vary/fail altogether)
 
-from: https://bblearn.merlin.mb.ca/bbcswebdav/xid-4317862_1
-Response code for this link: 302 ()
-Response code for m1_icon.gif: 304
+Usage instructions:
+  *<C:\\path\\to\\python\\>*python *<C:\\path\\to\\script\\>*convert-xid.py --fileRootPath *<path to course stored on HDD>* --webRootPath *<webdav address for course>*
 
-https://bblearn.merlin.mb.ca/bbcswebdav/courses/Physics40S_master_08/course_content/images/m1_icon.gif
+  - optional arguments after the previous line:
+    --debug         Shows certain information that could be beneficial for debugging
+    --verbose       Shows additional information for each replacement
+    --noWrite       Leaves the course untouched but looks through all of the html pages and identifies broken paths (and writes log)
+  - Note that path to python is optional if the path is part of your "path variable". The example assumes that python is in your
+    path. Please see the following for more information: https://geek-university.com/python/add-python-to-the-windows-path/
+
+Example:
+  python convert-xid.py --fileRootPath "C:\\Users\\MZielonko\\Documents\\Github\\Physics-40S" --webRootPath "https://bblearn.merlin.mb.ca/bbcswebdav/courses/Physics40S_ZielonkoMark_2019"
+
+Post-run instructions:
+  - Recommended to check a few files to make sure that there were no course-specific errors (probably won't be)
+  - Check the ErrorLog that was created with the date and time of the test being completed.
+    This contains any XID links that gave a "page not found" status - you will need to find these links manually
+    or simply delete them from the course
+  - Re-run the checker to make sure the files that had errors don't have other errors (the script stops looking
+    in a file after it encounters the first "bad link")
+
+Potential issues:
+  - If there is an error message about "unicode formatting", you may need to
+    change a python environment variable before re-running the script. See
+    screenshot of what that should look like in the file "EXAMPLE_EnvironmentVariableSet.jpg"
+
+  - Make sure that you are able to create files where the script resides; if
+    not, the script will be unable to create a log if there are broken links
+
+  - Make sure that the project you are editing is in an editable location; the
+    script will crash if it is unable to write changes to the file (unless in
+    "noWrite" mode)
--- convert-xid.py
+++ convert-xid.py
@@ -1,73 +1,293 @@
 import requests
-from webdav3.client import Client
+import os
+import re
+import getopt
+import sys
+import time
 
-webdav = None
+version = "1.3.6"
+verbose = False
+debug = False
+
+localFolderStructure = rootLocalFolder = fullLocalFolder = None
+webFolderStructure = rootWebFolder = None
+rootDir = courseAddr = None
+noWrite = False
+errorLog = []
+
+webDavPrefix = "https://bblearn.merlin.mb.ca/bbcswebdav/"
+
 
 def main():
-    global webdav
+    parseArgs()
+    parsePaths()
+    if debug:
+        print("Working directory:", os.getcwd())
+    print("-"*50)
 
-    webdavOpts = {
-        "webdav_hostname": "https://bblearn.merlin.mb.ca/bbcswebdav/courses/Physics40S_master_08",
-        "webdav_login": "markzielonko",
-        "webdav_password": ""
-    }
-    webdav = Client(webdavOpts)
-    webdav.verify = True
-    # currFolder = webdav.list()
-    # print(currFolder, "\n")
+    for dirName, subDirList, fileList in os.walk(rootDir):
+        for fname in fileList:
+            if fname.endswith(".html"):
+                fullPath = str(os.path.join(dirName, fname))
+                searchFile(fullPath)
 
-    searchFolder("")
-
-
-    print("\n\n")
-
-    # webpage = "https://bblearn.merlin.mb.ca/webapps/blackboard/execute/displayLearningUnit?content_id=_8456658_1&course_id=_12330_1&mode=view&framesetWrapped=true"
-    webpage = "https://bblearn.merlin.mb.ca/bbcswebdav/courses/Physics40S_master_08/course_content/module1/lesson1/intro.html"
-    picture = "https://bblearn.merlin.mb.ca/bbcswebdav/xid-4317862_1"
-    params = {}
+    if len(errorLog) > 0:
+        errLogToFile()
 
 
-    # Full page path made up of 3 parts: site location, folder location, file name
-    # Full image path made up of 3 parts: site location, folder location, file name
-    # Relative page path made up of 2 parts: difference between page and image folder locations, file name
+def parseArgs():
+    global localFolderStructure
+    global rootLocalFolder
+    global fullLocalFolder
+    global webFolderStructure
+    global rootWebFolder
+    global rootDir
+    global courseAddr
+    global noWrite
+    global verbose
+    global debug
 
-    siteLocn = "bblearn.merlin.mb.ca/bbcswebdav/courses/Physics40S_master_08/" # provided
-    folderLocn = "" # provided by link path (to be merged with siteLocn)
-    picResp = requests.get(picture, allow_redirects=False)
-    pageResp = requests.get(webpage, allow_redirects=False)
+    cmdArgs = sys.argv
+    argumentList = cmdArgs[1:]
+    if verbose:
+        print("Arguments: ", argumentList)
+    unixOptions = "f:w:hnvd"
+    gnuOptions = ["fileRootPath=", "webRootPath=", "noWrite", "help", "verbose", "debug"]
+
+    try:
+        arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
+    except getopt.error as error:
+        print(str(error))
+        sys.exit(-1)
+
+    if (len(arguments) == 0):
+        showHelpMsg()
+        sys.exit(0)
+
+    # Set values given by user
+    for arg, val in arguments:
+        if arg in ("-f", "--fileRootPath"):
+            rootDir = str(val)
+        elif arg in ("-w", "--webRootPath"):
+            courseAddr = str(val)
+        elif arg in ("-h", "--help"):
+            showHelpMsg()
+            sys.exit(0)
+        elif arg in ("-n", "--noWrite"):
+            print("NOWRITE: Not writing changes to files (test run)")
+            noWrite = True
+        elif arg in ("-v", "--verbose"):
+            print("VERBOSE: Verbose mode activated")
+            verbose = True
+        elif arg in ("-d", "--debug"):
+            print("DEBUG: Debug mode activated")
+            debug = True
+
+    # Dummy checks
+    if debug:
+        print("File Root Path:", rootDir)
+    if rootDir is None or not os.path.exists(rootDir):
+        print("Please specify a valid --fileRootPath (-f) and try again")
+        sys.exit(-2)
+    if courseAddr is None:
+        print("Please specify a --webRootPath (-w) and try again")
+        sys.exit(-3)
 
 
-    print(pageResp.status_code, picResp.status_code)
-    print(pageResp.history, picResp.history)
-    print(pageResp.headers.get("location"), picResp.headers.get("location"))
-    # for key, value in picResp.headers.items():
-    #     print(key, ": ", value)
-    for key, value in pageResp.headers.items():
-        print(key, ": ", value)
+def errLogToFile():
+    dateTime = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime(time.time()))
 
-def searchFolder(folderName):
-    print("folderName: ", folderName)
-    print(webdav.list(folderName))
-    print(webdav.list("course_content"))
-    print("\n\n")
-    for name in webdav.list(folderName)[1:]:
-        print("name: " + name)
-        basePath = webdav.get_url(folderName)
-        addition = "/" + name;
+    try:
+        file = open("ErrorLog_" + dateTime + ".txt", "xt", encoding="utf-8")
+        title = "Files that could not be completed due to links with redirect errors:"
+        file.write(title + "\n" + "="*len(title) + "\n\n")
+        for issueFile, link in errorLog:
+            file.write(issueFile + "\n\t" + link + "\n\n")
+        file.close()
+        print("\nCAUTION: Some XID links unable to be converted probably due to broken links.\n"
+              + "Output file \"ErrorLog_" + dateTime + ".txt\" has been written to the same file location of the script.\n"
+              + "Manually fix or remove links and run again to make sure there are no additional errors in these files.\n")
 
-        # if webdav.is_dir(basePath + addition):
-        #     print(fileObj)
-        # else:
-        #     print("(FILE) " + name)
-
-        # NOTE: using resources does not seem to work (can't give a valid file path)
+    except:
+        print("Unable to write output file. Check for permissions in script folder")
 
 
-    res1 = webdav.resource("https://bblearn.merlin.mb.ca/bbcswebdav/courses/Physics40S_master_08")
-    print(res1)
-    print (res1.check())
-    print(res1.info())
+
+def showHelpMsg():
+    print("convert-xid version " + str(version))
+    print("-----------------------------------------------")
+    print('Usage: python convert-xid.py -f | --fileRootPath "C:/Users/.../RootFolderName" -w | --webRootPath "http://bblearn.merlin.mb.ca/bbcswebdav/courses/COURSENAMEHERE" [-n | --noWrite] [-v | --verbose] [-d | --debug]')
+    print("Where: \n\tfileRootPath: On computer system, uppermost folder that is used for storage when downloading a course")
+    print("\twebRootPath: Path of course on the webserver (you can copy this directly from Blackboard, located under the heading \"Current Web Address\")\n")
+    print("\tnoWrite: Files are opened and scanned, but changes are not written back to files (will not check if your file is available for writing either)")
+    print("\tverbose: Changes are listed in more detail in the output window")
+    print("\tdebug: More information is provided in function ")
+    print()
+    print("Description:\nScans the entire folder structure of a local directory (fileRootPath) for HTML files. Checks each file for links that rely on Blackboard's XID system, finds their course-based path, and inserts a relative link to the same file based on the html file's location. This outcome allows courses to be exported to most LMS platforms and not rely on Blackboard-specific notations and lookup protocols.")
 
 
+def parsePaths():
+    global webFolderStructure
+    global rootWebFolder
+    global localFolderStructure
+    global rootLocalFolder
+    global fullLocalFolder
+
+    webFolderStructure, rootWebFolder = os.path.split(courseAddr)
+    if rootWebFolder == "":     # if ends in a slash (as folders are often written), goes one further level
+        webFolderStructure, rootWebFolder = os.path.split(webFolderStructure)
+    print("Root web folder: %s" %(rootWebFolder))
+
+    localFolderStructure, rootLocalFolder = os.path.split(rootDir)
+    if (rootLocalFolder == ""):     # same as rootWebFolder
+        localFolderStructure, rootLocalFolder = os.path.split(localFolderStructure)
+    print("Root local folder: %s" %(rootLocalFolder))
+    fullLocalFolder = os.path.join(localFolderStructure, rootLocalFolder)
+
+
+def getFilePathLevel(fileName):
+    # removes the first folder name
+    relPath = os.path.relpath(fileName, fullLocalFolder)
+    travPath, tempName = os.path.split(relPath)
+    pathLevel = 0
+
+    while (travPath != ""):
+        pathLevel += 1
+        travPath, tempName = os.path.split(travPath)
+
+    return pathLevel
+
+
+'''
+    Removes pieces that are common to both paths (traces back to closest ancestor)
+    @param fileName file on local system (full path)
+    @param linkAddr identifcal file's location through the webdav address
+'''
+def removeCommonPath(fileName, linkAddr):
+    global rootWebFolder
+
+    if verbose:
+        print("removeCommonPath: %s %s" % (fileName, linkAddr))
+    # get rid of anything further than the root folders
+    fileComponents = getPathComponents(fileName)
+    linkComponents = getPathComponents(linkAddr)
+
+    try:
+        linkComponents.index(rootWebFolder)
+
+    # if the web folder can't be found inside the full web path
+    except ValueError:
+        newRootWebFolder = linkComponents[linkComponents.index("bbcswebdav") + 2]
+
+        print("-"*50)
+        print(
+            """WARNING:\n\tThe redirect given by the server doesn't seem to reference the same course name.
+            Expected: %s\t\tGot: %s
+        This can happen if the course's name was manually changed after its creation.
+        Trying to use %s instead\n""" % (rootWebFolder, newRootWebFolder, newRootWebFolder))
+        print("-"*50)
+        # set the global root web folder to the presumed folder
+        rootWebFolder = newRootWebFolder
+
+    # remove anything preceding and including the root folder address
+    relevantFileComps = fileComponents[fileComponents.index(rootLocalFolder) + 1 : ]
+    relevantWebComps = linkComponents[linkComponents.index(rootWebFolder) + 1 : ]
+
+    while (relevantFileComps[0] == relevantWebComps[0]):
+        relevantFileComps = relevantFileComps[1:]
+        relevantWebComps = relevantWebComps[1:]
+
+    return [relevantFileComps, relevantWebComps]
+
+
+def getRelativePath(basis, link):
+    basisPath, linkPath = removeCommonPath(basis, link)
+    relativePath = ""
+
+    while True:
+        # if we're working in the folder's level
+        if (len(basisPath) == 1):
+            for path in linkPath:
+                relativePath = os.path.join(relativePath, path)
+            break
+        else:
+            relativePath = os.path.join("..\\", relativePath)
+            basisPath = basisPath[1:]
+
+    return relativePath.replace("\\", "/")
+
+
+def getPathComponents(folderStructure):
+    tempFolderStructure = "" + folderStructure
+    folders = []
+    tempFolderStructure, tempFolder = os.path.split(tempFolderStructure)
+    while tempFolder != "":
+        folders.insert(0, tempFolder)
+        tempFolderStructure, tempFolder = os.path.split(tempFolderStructure)
+
+    return folders
+
+
+def searchFile(fileName):
+    global errorLog
+
+    print("\tSEARCHING: %s" % (fileName))
+    file = open(fileName, "rt", encoding="utf-8")
+    try:
+        contents = file.read()
+    except UnicodeEncodeError as e:
+        print("\tERROR WRITING FILE: " + fileName)
+        print(e)
+    except UnicodeDecodeError as e:
+        print("\tERROR READING FILE: " + fileName)
+        print(e)
+    finally:
+        file.close()
+
+    regexLinkFind = "(http(s)?://bblearn.merlin.mb.ca)?/bbcswebdav/xid-[0-9_]*"
+    xid = re.search(regexLinkFind, contents)
+    while (xid):
+        if (verbose):
+            print("\tFOUND: %s (%s)" % (xid, fileName))
+        replaceStartIndex = xid.span()[0]
+        replaceEndIndex = xid.span()[1]
+
+        # look up the real link
+        xidLink = "xid" + xid.group(0).split("xid")[1]
+        if verbose:
+            print("\t\tSEARCHING FOR XID: %s" % (webDavPrefix + xidLink))
+        linkResp = requests.get(webDavPrefix + xidLink, allow_redirects=False)
+
+        if linkResp.status_code == 404:
+            print("ERROR: Link %s cannot be found. Please check file %s manually and re-run once all XID links can be found, or remove manually." % (webDavPrefix + xidLink, fileName))
+            errorLog.append((fileName, webDavPrefix + xidLink))
+            return False
+        # compare the relative file's path to the resulting link, only print necessary piece
+        # print(linkResp.status_code)
+        # print(linkResp.headers.get("location"))
+
+        newLink = getRelativePath(fileName, linkResp.headers.get("location"))
+        if verbose:
+            print("\t\tOLD: %s\tNEW: %s" %(linkResp.headers.get("location"), newLink))
+
+        if not verbose:
+            print("\t\tFOUND: %s\t NEW: %s" %(webDavPrefix + xidLink, newLink))
+
+        contents = contents[ :replaceStartIndex]  + newLink + contents[replaceEndIndex: ]
+         # check again
+        xid = re.search(regexLinkFind, contents)
+
+    if not noWrite:
+        file = open(fileName, "wt", encoding="utf-8")
+        try:
+            file.write(contents)
+        except UnicodeEncodeError as e:
+            print("\tERROR WRITING FILE: " + fileName)
+            print(e)
+        except UnicodeDecodeError as e:
+            print("\tERROR READING FILE: " + fileName)
+            print(e)
+        finally:
+            file.close()
 
 main()
